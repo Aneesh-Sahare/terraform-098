bf7660c740279f6bcb0eba3edc65f179a23b6591
Aneesh Sahare <145126838+Aneesh-Sahare@users.noreply.github.com>
2025-10-15 05:14:52 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,15 @@
-
+#KEY
 resource "aws_key_pair" "deployer" {
   key_name   = "terra-key-6867"
-  public_key = file("terra-key.pub")
+  public_key = file("/home/ubuntu/terra")
 }
 
+#VPC
 resource "aws_default_vpc" "default" {
 
 }
 
+#SG
 resource "aws_security_group" "allow_user_connect" {
   name        = "allow TLS"
   description = "Allow user to connect"
@@ -27,4 +29,22 @@ resource "aws_security_group" "allow_user_connect" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+#EC2
+resource "aws_instance" "testinstance" {
+  ami             = data.aws_ami.os_image.id
+  instance_type   = "t2.micro"  
+  key_name        = aws_key_pair.deployer.key_name                         #interpolation
+  security_groups = [aws_security_group.allow_user_to_connect.name]
+  tags = {
+    Name = "Terra-Automated"
+  }
+ 
+
+#COMMANDS
+ provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y" 
+    ]
   }
