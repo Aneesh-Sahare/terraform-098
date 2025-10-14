1723d4463fc191ea49b09a55fd968ec93ce19342
Aneesh Sahare <145126838+Aneesh-Sahare@users.noreply.github.com>
2025-10-15 05:02:50 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,30 @@
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "terra-key-6867"
+  public_key = file("terra-key.pub")
+}
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "allow_user_connect" {
+  name        = "allow TLS"
+  description = "Allow user to connect"
+  vpc_id      = aws_default_vpc.default.id
+  
+  ingress {
+    description = "port 22 allow"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = " allow all outgoing traffic "
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
