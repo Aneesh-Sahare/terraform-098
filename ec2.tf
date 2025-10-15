7e39e82e1d3e34bcc1c25b83fc21d545462aeca6
Aneesh Sahare <145126838+Aneesh-Sahare@users.noreply.github.com>
2025-10-15 05:51:13 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -35,12 +35,13 @@ resource "aws_security_group" "allow_user_connect" {
 # EC2 INSTANCE
 resource "aws_instance" "testinstance" {
   ami             = "ami-02d26659fd82cf299"
-  instance_type   = "t2.micro"
+  instance_type   = "t3.micro"
   key_name        = aws_key_pair.deployer.key_name
   security_groups = [aws_security_group.allow_user_connect.name]
 
   tags = {
     Name = "Terra-Automated"
-  }  
+  }
+
 }
 
